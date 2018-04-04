490c0316c9123294c6e66a27544035ada9f8485c
Daniel Suciu <suciudani@gmail.com>
2018-04-04 23:58:11 +0300

    the star

--- LABS/3/chess.js
+++ LABS/3/chess.js
@@ -1,25 +1,12 @@
-function getRow(white, black) {
-    return "<tr><td>" + white + "</td><td>" + black + "</td></tr>";
+var size = 25;
+for(i = 0; i< size; i++){
+    if(i % 2 == 0){
+        document.write("<td id='tdblack'></td>");
+    }
+    else{
+        document.write("<td id='tdwhite'></td>");
+    }
+    if( (i+1) % 5 == 0){
+        document.write("<tr>");
+    }
 }
-
-var colors = [
-    ["white", "black"],
-    ["black", "white"],
-    ["white", "black"]
-];
-
-var rows = '';
-
-// colors.forEach(function(color) {
-//     rows += getRow(color[0], color[1]); // 0 - prima coloana, 1 - a doua coloana
-// });
-
-for (var i = 0; i < colors.length; i ++) {
-    var color = colors[i];
-    rows += getRow(color[0], color[1]);
-}
-
-$('#table_1 tbody').html(rows);
-
-rows = '';
-
